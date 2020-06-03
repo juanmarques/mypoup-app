385be344e22e815c172184ade3c9d7e07321592a
Reinaldo Nani <reinaldo@mimobr.com>
2020-06-04 00:15:32 +0100

    2020 06 04 - login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignComponent } from './sign/sign.component';
+
+import { FormsModule } from '@angular/forms';/* Forms  */
+import { MatInputModule } from '@angular/material/input'/* Material Input */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
